f88af279b5ff46fa55f177f4b32624b4798f2865
usman-khan98 <88879256+usman-khan98@users.noreply.github.com>
2022-10-26 12:13:58 +0500

    Update UserTable.jsx

--- src/components/userTable/UserTable.jsx
+++ src/components/userTable/UserTable.jsx
@@ -6,8 +6,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import "../table/table.css";
-
+// import "../table/table.css";
+import "../userTable/table.css";
 
 export default function UserTable() {
   
